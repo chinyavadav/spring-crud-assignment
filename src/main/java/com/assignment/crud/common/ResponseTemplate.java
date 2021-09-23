7b55a58cbd4d0d3f542a98f8b1e8f2b8b21ac884
Victor Chinyavada <chinyavadav@gmail.com>
2021-09-23 19:46:45 +0200

    add generic ResponseTemplate

---
+++ src/main/java/com/assignment/crud/common/ResponseTemplate.java
@@ -0,0 +1,40 @@
+package com.assignment.crud.common;
+
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ResponseBody
+public class ResponseTemplate<T> {
+    private String status;
+    private String message;
+    private T data;
+
+    public ResponseTemplate(String status, String message, T data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
