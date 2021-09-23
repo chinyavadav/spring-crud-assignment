910928dc8a121d89bdaab79eeb29a12e8729cdd2
Victor Chinyavada <chinyavadav@gmail.com>
2021-09-23 19:46:05 +0200

    add CustomException and CRUDService

---
+++ src/main/java/com/assignment/crud/common/CRUDService.java
@@ -0,0 +1,16 @@
+package com.assignment.crud.common;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface CRUDService<T> {
+    T saveOrUpdate(T object);
+
+    T findById(UUID id);
+
+    List<T> findByField(String field, Object value);
+
+    PaginatedDto<T> getPaginated(int page, int size);
+
+    boolean delete(UUID id);
+}
